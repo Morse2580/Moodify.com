417ed566b48aa7b1a0da9aabda28c0e5a99f826d
moses njau <r0853097@student.thomasmore.be>
2021-04-01 14:59:37 +0200

    Initial commit

---
+++ include/nav.php
@@ -0,0 +1,29 @@
+<nav>
+    <div class="logo">
+        <h4>Moodify</h4>
+    </div>
+
+    <!-- Nav links -->
+    <ul class="nav-links">
+        <li>
+            <a href="">Home</a>
+        </li>
+        <li>
+            <a href="">About</a>
+        </li>
+        <li>
+            <a href="">Work</a>
+        </li>
+        <li>
+            <a href="">Projects</a>
+        </li>
+    </ul>
+
+
+    <!--responsive nav -->
+    <div class="burger">
+        <div class="line_one"></div>
+        <div class="line_two"></div>
+        <div class="line_three"></div>
+    </div>
+</nav>
